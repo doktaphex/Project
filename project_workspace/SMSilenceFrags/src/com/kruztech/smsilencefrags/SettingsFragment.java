aad4971fb73e34b8f9d335245b97fe57317c6719
doktaphex <jmk@kruztech.com>
2014-02-26 11:36:16 +0000

    New project
    Updated previous App to use fragments.  Added slide out menu to conform
    with Google UI guidelines.

---
+++ project_workspace/SMSilenceFrags/src/com/kruztech/smsilencefrags/SettingsFragment.java
@@ -0,0 +1,168 @@
+package com.kruztech.smsilencefrags;
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.Toast;
+
+public class SettingsFragment extends Fragment {
+
+	private Switch serviceSwitch;
+	private Switch bootSwitch;
+	private Switch notificationSwitch;
+	public static final String Service = "serviceKey";
+	public static final String Boot = "bootKey";
+	public static final String Noti = "notiKey";
+	public static final String MyPreferences = "AppPrefs";
+	SharedPreferences settings;
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+		return inflater.inflate(R.layout.fragment_settings, container, false);
+	}
+	
+	@Override
+	public void onStart() {
+		super.onStart();
+		initControls();
+		settingsLogic();
+	}
+
+	protected void initControls(){
+		serviceSwitch = (Switch) getView().findViewById(R.id.serviceSwitch);
+		bootSwitch = (Switch) getView().findViewById(R.id.bootSwitch);
+		notificationSwitch = (Switch) getView().findViewById(R.id.notificationSwitch);
+		bootSwitch.setEnabled(false);
+		notificationSwitch.setEnabled(false);
+	}
+
+	protected void settingsLogic(){
+		settings = getActivity().getSharedPreferences(MyPreferences, Context.MODE_PRIVATE);
+
+		if (settings.contains(Service))
+		{
+			serviceSwitch.setChecked(true);
+			bootSwitch.setEnabled(true);
+			notificationSwitch.setEnabled(true);
+		}
+		if (settings.contains(Boot))
+		{
+			bootSwitch.setChecked(true);
+		}
+		if (settings.contains(Noti))
+		{
+			notificationSwitch.setChecked(true);
+		}
+
+		serviceSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+			@Override
+			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+				// TODO Auto-generated method stub
+				if (isChecked) {
+					bootSwitch.setEnabled(true);
+					notificationSwitch.setEnabled(true);
+					Editor edit = settings.edit();
+					edit.putBoolean("serviceKey", true);
+					edit.apply();
+
+					Toast.makeText(getActivity().getApplicationContext(), 
+							"Service Started", Toast.LENGTH_SHORT).show();
+				}else{
+					bootSwitch.setEnabled(false);
+					notificationSwitch.setEnabled(false);
+					Editor edit = settings.edit();
+					edit.remove("serviceKey");
+					edit.apply();
+
+					Toast.makeText(getActivity().getApplicationContext(), 
+							"Service Stopped", Toast.LENGTH_SHORT).show();
+				}
+
+			}
+		});
+
+		bootSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+			@Override
+			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+				// TODO Auto-generated method stub
+				if (isChecked) {
+					Editor edit = settings.edit();
+					edit.putBoolean("bootKey", true);
+					edit.apply();
+					Toast.makeText(getActivity().getApplicationContext(), 
+							"Service will start at device bootup", Toast.LENGTH_SHORT).show();
+				}else{
+					Editor edit = settings.edit();
+					edit.remove("bootKey");
+					edit.apply();
+					Toast.makeText(getActivity().getApplicationContext(), 
+							"Service will NOT start at device bootup", Toast.LENGTH_SHORT).show();
+				}
+
+			}
+		});
+
+		notificationSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+			@Override
+			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+				// TODO Auto-generated method stub
+				if (isChecked) {
+					Editor edit = settings.edit();
+					edit.putBoolean("notiKey", true);
+					edit.apply();
+					Toast.makeText(getActivity().getApplicationContext(), 
+							"Service will display notifications", Toast.LENGTH_SHORT).show();
+				}else{
+					Editor edit = settings.edit();
+					edit.remove("notiKey");
+					edit.apply();
+					Toast.makeText(getActivity().getApplicationContext(), 
+							"Service will NOT display notifications", Toast.LENGTH_SHORT).show();
+				}
+
+			}
+		});
+	}
+
+//	@Override
+//	protected void onResume() {
+//		super.onResume();
+//		initControls();
+//		settingsLogic();
+//	}
+//
+//	@Override
+//	protected void onPause() {
+//		super.onPause();
+//	}
+//
+//	@Override
+//	public boolean onCreateOptionsMenu(Menu menu) {
+//		// Inflate the menu; this adds items to the action bar if it is present.
+//		getMenuInflater().inflate(R.menu.main, menu);
+//		return true;
+//	}
+//
+//	public boolean onOptionsItemSelected(MenuItem item) {
+//
+//		switch (item.getItemId()) {
+//		case R.id.about:
+//			startActivity(new Intent(this, AboutActivity.class));
+//			return true;
+//		case R.id.website:
+//			Toast.makeText(getApplicationContext(), 
+//					"WWW.KRUZTECH.COM", Toast.LENGTH_LONG).show();
+//			return true;
+//		default:
+//			return super.onOptionsItemSelected(item);
+//		}
+//
+//	}
+}
