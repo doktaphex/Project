32c5ff4d07b07458c8c1e3dd2dd752581648e6b6
doktaphex <jmk@kruztech.com>
2014-03-07 20:35:42 +0000

    added code, but need to remove whole service

--- project_workspace/SMSilenceFrags/src/com/kruztech/smsilencefrags/TelService.java
+++ project_workspace/SMSilenceFrags/src/com/kruztech/smsilencefrags/TelService.java
@@ -1,37 +1,49 @@
 package com.kruztech.smsilencefrags;
 
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
 import android.os.IBinder;
 import android.telephony.PhoneStateListener;
 import android.telephony.TelephonyManager;
-import android.widget.Toast;
+import android.util.Log;
 
 public class TelService extends Service {
-	public TelService() {
-	}
+	private static final String TAG = "Kruztech";
+	String incomingNumber = "number";
+	private TelephonyManager telephonyManager;
 
 	PhoneStateListener callStateListener = new PhoneStateListener(){
+		
 		public void onCallStateChanged(int state, String incomingNumber){
-			String callStateStr = "Unknown";
-			
+			telephonyManager.listen(callStateListener,PhoneStateListener.LISTEN_CALL_STATE);
 			switch(state){
 			case TelephonyManager.CALL_STATE_IDLE :
-				callStateStr = "idle"; break;
-				case TelephonyManager.CALL_STATE_OFFHOOK :
-				callStateStr = "offhook"; break;
-				case TelephonyManager.CALL_STATE_RINGING :
-				callStateStr = "ringing. Incoming number is: "
-				+ incomingNumber;
 				break;
+				case TelephonyManager.CALL_STATE_OFFHOOK :
+				break;
+				case TelephonyManager.CALL_STATE_RINGING :
+					Intent accelIntent = new Intent(getBaseContext(), AccelService.class);
+					startService(accelIntent);
+					Log.d(TAG,"TelReceiver: Accelerometer and Telephony services started");
+
+					incomingNumber = accelIntent.getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER);
+					
+						Intent i = new Intent(getBaseContext(), AccelService.class);
+						i.putExtra("com.kruztech.smsilencefrags.INCOMING_NUMBER", incomingNumber);
+						startService(i);
+						Log.d(TAG,"TelReceiver: data passed?");
+				//break;
 				default : break;
 			}
 			
 		}
 	};
 	
-//	telephonyManager.listen(callStateListener,
-//			PhoneStateListener.LISTEN_CALL_STATE);
+	@Override
+	public void onStart(Intent accelIntent, int startId){
+		telephonyManager = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+	}
 	
 	@Override
 	public IBinder onBind(Intent intent) {
