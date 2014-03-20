67c4f7bf787e9e1944b30044bbc897bd57bac5f1
doktaphex <jmk@kruztech.com>
2014-03-20 14:38:06 +0000

    added messaging fragment

---
+++ project_workspace/YakChat/src/com/kruztech/yakchat/MessagingFrag.java
@@ -0,0 +1,26 @@
+package com.kruztech.yakchat;
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link android.support.v4.app.Fragment} subclass.
+ * 
+ */
+public class MessagingFrag extends Fragment {
+
+	public MessagingFrag() {
+		// Required empty public constructor
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		// Inflate the layout for this fragment
+		return inflater.inflate(R.layout.fragment_messaging, container, false);
+	}
+
+}
